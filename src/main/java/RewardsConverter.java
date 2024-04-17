a672a720ee48c29890f51fdb3d09064cc01c1320
itsjahaziel <jahazielr116@gmail.com>
2024-04-17 00:44:06 -0400

    Rewards Credit card

---
+++ src/main/java/RewardsConverter.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class RewardsConverter {
+    public static void main(String[] args) {
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("Welcome to the Credit Card Rewards Converter!");
+            System.out.println("Please enter a cash value to convert to airline miles: ");
+            var input_value = scanner.nextLine();
+            double cashValue;
+            try {
+                cashValue = Double.parseDouble(input_value);
+            } catch (NumberFormatException exception) {
+                System.out.println("Could not parse input value as a double, exiting");
+                return;
+            }
+            System.out.println("converting $" + input_value + " to miles");
+            var rewardsValue = new RewardValue(cashValue);
+            System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+        }
+    }
+}
+
+class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
